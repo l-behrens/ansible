e6a97b7a0a8ee22a4f15aa75a07872b7addbada6
Lahrs Behrens <behrens_lars@gmx.de>
2019-04-12 19:00:16 +0200

    * added deletion function * major bugfixes * tested against local redis

--- lib/ansible/modules/database/redis/redis_access_module.py
+++ lib/ansible/modules/database/redis/redis_access_module.py
@@ -80,8 +80,9 @@ except ImportError:
 else:
     redis_found = True
 
+
 def get_val(r, keys):
-    return [r.get(key) for key in keys]
+    return str({key: r.get(key) for key in keys})
 
 def dump(r):
     res={}
@@ -89,86 +90,84 @@ def dump(r):
         res[key] = r.get(key)
     return res
 
-def set_key_val(r, timeout, key_val):
+def set_key_val(r, key_val):
     try:
-        with r.lock('my-lock-key', blocking_timeout=timeout) as lock:
+        with r.lock('my-lock-key', blocking_timeout=5) as lock:
             for key in key_val:
                 r.set(key, key_val[key])
     except Exeption:
         module.fail_json(msg='Locking redis failed', **result)
+
 def init_redis_client(host='localhost', port=6379, db=0):
     return redis.Redis(host=host, port=port, db=db)
 
 def run_module():
-    if not redis_found:
-        module.fail_json(msg="the python redis module is required")
 
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
         port=dict(type='str', required=False, default='6379'),
         host=dict(type='str', required=False, default='127.0.0.1'),
         db=dict(type='int', required=False, default=0),
-        set_key_val=dict(type='dict', required=False),
-        get_val=dict(type='list', required=False),
-        mode=dict(default="get", choices=["get", "set","dump", ]),
+        set=dict(type='dict', required=False),
+        get=dict(type='list', required=False),
+        keys=dict(type='list', required=False),
+        mode=dict(default="get", choices=["get", "set","dump", "delete"]),
         ssl_cert=dict(default=None, type='path'),
         ssl_key=dict(default=None, type='path'),
         ssl_ca=dict(default=None, type='path'),
         connect_timeout=dict(default=5, type='int'),
     )
 
-    r = init_redis_client(
-        module.params['host'],
-        module.params['port'],
-        module.params['db'])
-
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # change is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
     result = dict(
         changed=False,
         original_message='',
         message=''
     )
 
-
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
+    if not redis_found:
+        module.fail_json(msg="the python redis module is required")
 
-    if module.params['mode'] is 'dump':
-        result[message] = dump(r)
+    r = init_redis_client(
+        module.params['host'],
+        module.params['port'],
+        module.params['db'])
 
-    elif module.params['mode'] is 'read':
-        result[message] = get_val(get_val)
+    result['message']=module.params['mode']
+    if module.params['mode'] == 'dump':
+        result['message'] = dump(r)
+    elif module.params['mode'] == 'get':
+        if not module.params['get']:
+            module.fail_json(msg='You must define get: <key> Parameter when mode is get', **result)
+        result['message']= get_val(r, module.params['get'])
 
-    elif module.params['mode'] is 'write':
-        result[message] = 0
+    elif module.params['mode'] == 'set':
+        if not module.params['set']:
+            module.fail_json(msg='You must define set: {key: value} Parameter when mode is get', **result)
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
+        result['message'] = "running in checkmode. Set Nothing"
+
+    elif module.params['mode'] == 'delete':
+        if not module.params['keys']:
+            module.fail_json(msg='You must define keys Parameter when mode is get', **result)
+
+        result['message'] = "running in checkmode. Delete Nothing"
+
     if module.check_mode:
         return result
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    if module.params['mode'] is 'write':
-        result[message] = set_key_val(r, module.params['set_key_val'])
-        result[changed] = True
+    if module.params['mode'] == 'set':
+        result['message'] = set_key_val(r, module.params['set'])
+        result['changed'] = True
 
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
+    elif module.params['mode'] == 'delete':
+        for i in module.params['keys']:
+            ret=r.delete(i)
+        result['message'] = 'deleted...'
+        result['changed'] = True
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
